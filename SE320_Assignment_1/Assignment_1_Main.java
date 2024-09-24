5b51fa5e61d156706033d2a3215166cab7926313
henryspacer <henrycsanders03@gmail.com>
2024-09-23 22:49:23 -0400

    HW Assignment 1 Github Test part 2

--- SE320_Assignment_1/Assignment_1_Main.java
+++ SE320_Assignment_1/Assignment_1_Main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Assignment_1_Main {
-
     public static void main(String[] args) {
+        Part1();
+        Part2();
+    }
+    public static void Part1() {
         Scanner scanner = new Scanner(System.in);
         int num1 = 0, num2 = 0;
         boolean validInput = false;
@@ -31,5 +35,25 @@ public class Assignment_1_Main {
 
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+    public static void Part2() {
+        int[] numbers = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(1000);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an index between 0 and 99: ");
+        try {
+            int index = scanner.nextInt();
+
+            System.out.println("Element at index " + index + ": " + numbers[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Out of Bounds");
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter an integer.");
+        }
     }
 }
